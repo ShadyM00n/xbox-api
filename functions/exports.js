0a68ca4c6707f8d6387dc24b92544a0a7bc5fde2
ShadyMoon <103329255+ShadyM00n@users.noreply.github.com>
2024-09-15 12:11:34 -0400

    Create exports.js

---
+++ functions/exports.js
@@ -0,0 +1,13 @@
+const getPROFILE = require('./getPROFILE');
+const getGAMES = require('./getGAMES');
+const getXUID = require('./getXUID');
+const getUSERNAME = require('./getUSERNAME');
+const getACTIVITY = require('./getACTIVITY');
+
+module.exports = { 
+    getPROFILE,
+    getGAMES,
+    getXUID,
+    getUSERNAME,
+    getACTIVITY
+}
